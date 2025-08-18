34e9bf0be70d98d03cbb9fabfe9dd0fa4187d772
Chinmoy Mandal <chinmoymandal925807@gmail.com>
2025-08-19 00:36:49 +0530

    creating envorment for create portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 // import {style} from './App.module.css'
+import './x.css';
+
+// import { useState } from "react"
 
 function App() {
+  // const [mode, setmode] = useState('light');
 
   return (
     < >
-      
+      Hello
     </>
   )
 }
