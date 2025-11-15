9ca265b170861b6bbf53bb5870cdef570b2e6f9f
Chinmoy Mandal <chinmoymandal925807@gmail.com>
2025-11-16 01:11:38 +0530

    text-font update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <section id="about" className="m-5 max-[400px]:mx-0">
       <h2 id="about_main" className="text-4xl pl-5">Know Who <span className="text-[var(--text_highlight)]">
         I'M</span></h2>
-      <pre className="text-2xl p-5 max-[400px]:px-0 whitespace-break-spaces">
+      <pre className="text-2xl p-5 max-[400px]:px-0 max-[400px]:text-xl whitespace-break-spaces">
         <span className="font-bold text-[var(--text_highlight)] text-shadow-xs text-shadow-gray-500">Hello!   </span>
         I am <span className="font-bold text-[var(--text_highlight)] text-shadow-xs text-shadow-gray-500">Chinmoy Mandal</span> from <span className="font-bold text-[var(--text_highlight)]">Digha, India.</span>
         I'm a passionate developer with experience in building
